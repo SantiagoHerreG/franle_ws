5d4fb665d2f406506428205ef33fa6b14de977ae
SantiagoHerreG <888@holbertonschool.com>
2020-03-05 04:19:08 +0000

    app_socket.js: new features added: candidates object to contain all posible connections, random matchin between 20 first users waiting, correct close connection hadling

--- README.md
+++ README.md
@@ -5,9 +5,16 @@
 ### Web sockets Server Usage
 run with `npm start`
 
-install wscat with `wscat install -g wscat`
+install ws with `npm install -g ws`
+install wscat with `npm install -g wscat`
 
 Connect to the server using `wscat -c ws://0.0.0.0:12345`
 
 The first message should be a json format string containing native language and new language:
-e.g.:`{"nativeLang":"esp", "newLang":"eng"}`
+e.g.:`{"nativeLang":"spa", "newLang":"eng"}`
+
+Allowed language codes:
++ spa
++ eng
++ deu
++ fr
--- app_socket.js
+++ app_socket.js
@@ -1,14 +1,28 @@
 const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 let connections = {};
-let freeConn = {};
 let match = {};
+let candidates = {
+  "engTospa": [],
+  "spaToeng": [],
+  "frToeng": [],
+  "engTofr": [],
+  "deuTofr": [],
+  "frTodeu": [],
+  "spaTofr": [],
+  "spaTodeu": [],
+  "engTodeu": [],
+  "frTospa": [],
+  "deuTospa": [],
+  "deuToeng": [],
+}
+let matchingProfile = "";
+let userProfile = "";
 
 const wss = new WebSocket.Server({
   port: 12345,
   clientTracking: true
   /* Use verifyClient for allowing the connections */
-
 }, () => {
   console.log('Server started...');
   console.log('Listening on port 12345');
@@ -18,33 +32,46 @@ wss.on('connection', (socket, req) => {
   const id = uuidv4();
   connections[id] = socket;
   socket.id = id;
-
+  console.log(req);
   console.log(`Client ${id} connected...`)
 
   socket.on('message', data => {
     console.log(`Client ${id}: ${data}`);
-    if (!freeConn[id] && !match[id]) {
+
+    if (!socket.free && !match[id]) {
       data = JSON.parse(data);
-      freeConn[id] = [data.nativeLang, data.newLang];
-      for (var [key, value] of Object.entries(freeConn)) {
-        if (key != id && value[1] === data.nativeLang && value[0] === data.newLang) {
-          match[id] = key;
-          match[key] = id;
-          delete freeConn[id];
-          console.log("Matched!");
-          socket.send("id", socket.id);
-          socket.send("paired", key);
-          return;
-        }
+      userProfile = data.nativeLang + "To" + data.newLang;
+      socket.userProfile = userProfile;
+      matchingProfile = data.newLang + "To" + data.nativeLang;
+
+      if (candidates[matchingProfile].length) {
+        candPool = Math.min(20, candidates[matchingProfile].length);
+        keyPlace = Math.floor(Math.random() * (candPool));
+        key = candidates[matchingProfile][keyPlace];
+        match[id] = key;
+        match[key] = id;
+        /*Delete the key from candidates*/
+        candidates[matchingProfile].splice(keyPlace, 1);
+        console.log("Matched!");
+        socket.send("Local ID and paired ID");
+        socket.send(socket.id);
+        socket.send(key);
+        socketPaired = connections[key];
+        socketPaired.send("Local ID and paired ID");
+        socketPaired.send(key);
+        socketPaired.send(socket.id);
+        return;
       }
       console.log("new user and no match found");
+      candidates[userProfile].push(id);
+      socket.free = true;
     } else if (match[id]) {
       pairedId = match[id];
       pairedSocket = connections[pairedId];
       pairedSocket.send(data);
       console.log("data sent");
     }
-    console.log("freeconn", freeConn);
+    console.log("candidates", candidates);
     console.log("match", match);
   });
 
@@ -55,5 +82,19 @@ wss.on('connection', (socket, req) => {
 
   socket.on('close', _ => {
     console.log(`Client ${id} leaving...`)
+    delete connections[id];
+    if (match[id]) {
+      pairedId = match[id];
+      pairedSocket = connections[pairedId];
+      pairedSocket.close(1000, "User2 has left");
+      delete match[id];
+      delete match[pairedId];
+      delete connections[pairedId]
+    } else {
+      idx = candidates[socket.userProfile].indexOf(id);
+      if (idx > -1) {
+        candidates[socket.userProfile].splice(idx, 1);
+      }
+    }
   });
 });
